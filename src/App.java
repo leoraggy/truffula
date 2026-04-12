1590526d71d21c0d87ae02b02e00dd4ea9ac0195
leoraggy <leoragual35@gmail.com>
2026-04-11 19:26:07 -0700

    Completed wave 3

--- src/App.java
+++ src/App.java
@@ -41,11 +41,6 @@ public class App {
    * Error messages will be shown for illegal arguments or a not found file
    */
   public static void main(String[] args) throws Exception {
-    // TODO: Implement this
-    // You should create a TruffulaOptions object using the args and
-    // pass it to a new TruffulaPrinter that uses System.out
-    // Then, call printTree on the TruffulaPrinter
-      System.out.println(args.length);
       TruffulaOptions truffulaOptions = new TruffulaOptions(args);
 
       TruffulaPrinter printer = new TruffulaPrinter(truffulaOptions);
